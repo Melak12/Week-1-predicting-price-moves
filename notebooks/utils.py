6d71ee6acb9d67630d2f4c6ab6d2b3d2e48bd62d
Melake Wubshet <wubshetmelake@gmail.com>
2025-06-05 16:59:48 +0300

    feat: update README.md with dataset structure and major works; refactor load_data function to use a more general base directory

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def load_data(filename, base_dir="../data/yfinance_data"):
+def load_data(filename, base_dir="../data"):
     """Load CSV data from the base data directory."""
     filepath = f"{base_dir}/{filename}"
     df = pd.read_csv(filepath)
